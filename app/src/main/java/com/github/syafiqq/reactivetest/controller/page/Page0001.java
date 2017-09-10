48eb54ff80fb57a7067114841c37d23daf12f64b
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-09-10 20:42:18 +0700

    Remvoe overriding

--- app/src/main/java/com/github/syafiqq/reactivetest/controller/page/Page0001.java
+++ app/src/main/java/com/github/syafiqq/reactivetest/controller/page/Page0001.java
@@ -1,12 +1,10 @@
 package com.github.syafiqq.reactivetest.controller.page;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
 import com.github.syafiqq.reactivetest.R;
 import com.github.syafiqq.reactivetest.controller.DrawerActivity;
 
@@ -39,58 +37,5 @@ public class Page0001 extends DrawerActivity implements NavigationView.OnNavigat
         {
             super.onBackPressed();
         }
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if(id == R.id.action_settings)
-        {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item)
-    {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if(id == R.id.nav_camera)
-        {
-            // Handle the camera action
-        }
-        else if(id == R.id.nav_gallery)
-        {
-
-        }
-        else if(id == R.id.nav_slideshow)
-        {
-
-        }
-        else if(id == R.id.nav_manage)
-        {
-
-        }
-        else if(id == R.id.nav_share)
-        {
-
-        }
-        else if(id == R.id.nav_send)
-        {
-
-        }
-
-        super.drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
