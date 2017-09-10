f9a560e1f41db7e844f4ed0e8d9c4624f817a62c
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-09-10 22:36:15 +0700

    Completing Page0001

--- app/src/main/java/com/github/syafiqq/reactivetest/controller/page/Page0001.java
+++ app/src/main/java/com/github/syafiqq/reactivetest/controller/page/Page0001.java
@@ -7,17 +7,32 @@ import android.support.v4.view.GravityCompat;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import com.github.javafaker.Faker;
 import com.github.syafiqq.reactivetest.R;
 import com.github.syafiqq.reactivetest.controller.DrawerActivity;
+import io.reactivex.Observable;
+import io.reactivex.functions.Consumer;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Page0001 extends DrawerActivity implements NavigationView.OnNavigationItemSelectedListener
 {
+    @BindView(R.id.page0001_list_view_data_container) public ListView displayData;
+    private List<String> dataContainer;
+    private ArrayAdapter<String> listAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_0001);
+        ButterKnife.bind(this, super.drawer);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.page_0001_toolbar);
         setSupportActionBar(toolbar);
 
@@ -26,6 +41,26 @@ public class Page0001 extends DrawerActivity implements NavigationView.OnNavigat
         toggle.syncState();
 
         super.navigation.setNavigationItemSelectedListener(this);
+
+        this.dataContainer = new LinkedList<>();
+
+        this.listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, this.dataContainer);
+        this.displayData.setAdapter(this.listAdapter);
+    }
+
+    @Override protected void onStart()
+    {
+        super.onStart();
+    }
+
+    @Override protected void onStop()
+    {
+        super.onStop();
+    }
+
+    @Override protected void onDestroy()
+    {
+        super.onDestroy();
     }
 
     @Override
@@ -60,5 +95,28 @@ public class Page0001 extends DrawerActivity implements NavigationView.OnNavigat
 
         this.drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @OnClick(R.id.page0001_button_generate_data)
+    public void generate()
+    {
+        Faker faker = new Faker();
+        List<String> _tempData = new LinkedList<>();
+        for(int i = -1, is = 50; ++i < is; )
+        {
+            _tempData.add(faker.name().fullName());
+        }
+        this.dataContainer.clear();
+        this.listAdapter.notifyDataSetChanged();
+
+
+        Observable.fromArray(_tempData.toArray(new String[_tempData.size()])).subscribe(new Consumer<String>()
+        {
+            @Override public void accept(String s) throws Exception
+            {
+                Page0001.this.dataContainer.add(s);
+                Page0001.this.listAdapter.notifyDataSetChanged();
+            }
+        });
     }
 }
