47b53e8aa778459f11baaa3098c8f3b908b37353
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-09-10 20:42:05 +0700

    Enhance Drawer Activity

--- app/src/main/java/com/github/syafiqq/reactivetest/controller/DrawerActivity.java
+++ app/src/main/java/com/github/syafiqq/reactivetest/controller/DrawerActivity.java
@@ -1,14 +1,17 @@
 package com.github.syafiqq.reactivetest.controller;
 
 import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.widget.FrameLayout;
 import com.github.syafiqq.reactivetest.R;
 import timber.log.Timber;
 
-public class DrawerActivity extends AppCompatActivity
+public class DrawerActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener
 {
     protected DrawerLayout drawer;
     protected NavigationView navigation;
@@ -21,7 +24,31 @@ public class DrawerActivity extends AppCompatActivity
         this.navigation = drawer.findViewById(R.id.navigation_view);
         final FrameLayout container = drawer.findViewById(R.id.drawer_container);
 
+        this.navigation.setNavigationItemSelectedListener(this);
+
         super.getLayoutInflater().inflate(layoutResID, container, true);
         super.setContentView(drawer);
+    }
+
+    @Override public boolean onNavigationItemSelected(@NonNull MenuItem item)
+    {
+        // Handle navigation view item clicks here.
+        final int id = item.getItemId();
+
+        switch(id)
+        {
+            case R.id.drawer_menu_page_0001:
+            {
+                Timber.d("Page 0001");
+            }
+            break;
+            default:
+            {
+                Timber.d("Can't Understand");
+            }
+        }
+
+        this.drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
