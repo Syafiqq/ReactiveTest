c3eae8f6cc36d63fee6998c32a3b25ee0d5060ac
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-10-02 12:44:51 +0700

    Initial Phase of Create EmptyNeverError

---
+++ app/src/main/java/com/github/syafiqq/reactivetest/controller/page/operators/create/OpCreateEmptyNeverError.java
@@ -0,0 +1,34 @@
+package com.github.syafiqq.reactivetest.controller.page.operators.create;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import com.github.syafiqq.reactivetest.R;
+
+public class OpCreateEmptyNeverError extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_op_create_empty_never_error);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
