ad84075af992baebfeae825d72b583205dcb4287
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-10-06 09:38:30 +0700

    Completing from

--- app/src/main/java/com/github/syafiqq/reactivetest/controller/page/operators/create/OpCreateFrom.java
+++ app/src/main/java/com/github/syafiqq/reactivetest/controller/page/operators/create/OpCreateFrom.java
@@ -1,6 +1,7 @@
 package com.github.syafiqq.reactivetest.controller.page.operators.create;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -9,6 +10,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import butterknife.ButterKnife;
 import com.github.syafiqq.reactivetest.R;
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Action;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
 import timber.log.Timber;
 
 public class OpCreateFrom extends DrawerOpCreateActivity implements NavigationView.OnNavigationItemSelectedListener
@@ -28,6 +36,38 @@ public class OpCreateFrom extends DrawerOpCreateActivity implements NavigationVi
         toggle.syncState();
 
         super.navigation.setNavigationItemSelectedListener(this);
+        FutureTask<Long> task = new FutureTask<>(new Callable<Long>()
+        {
+            @Override public Long call() throws Exception
+            {
+                SystemClock.sleep(2000);
+                return Math.round(Math.random() * Byte.MAX_VALUE);
+            }
+        });
+
+        Observable<Long> observable = Observable.fromFuture(task, Schedulers.newThread()).observeOn(AndroidSchedulers.mainThread());
+
+        task.run();
+
+        observable.subscribe(new Consumer<Long>()
+        {
+            @Override public void accept(Long aLong) throws Exception
+            {
+                Timber.d("onNext" + aLong);
+            }
+        }, new Consumer<Throwable>()
+        {
+            @Override public void accept(Throwable throwable) throws Exception
+            {
+                Timber.e(throwable);
+            }
+        }, new Action()
+        {
+            @Override public void run() throws Exception
+            {
+                Timber.d("onComplete");
+            }
+        });
     }
 
 
